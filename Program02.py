4c9098a4e2420aacb855a5d14d08b18ffe5c06f7
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-13 11:08:48 +0800

    Final commit with spacing correction

--- Program02.py
+++ Program02.py
@@ -3,7 +3,7 @@ b = int(input("Second number: "))
 c = int(input("Third number: "))
 
 if a < b and a < c:
-    print(f"The lowest number is{a}.")
+    print(f"The lowest number is {a}.")
 elif b < a and b < c:
     print(f"The lowest number is {b}.")
 else:
