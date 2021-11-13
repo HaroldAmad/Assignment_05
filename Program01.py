4179d52aba704172e95792b31c9a975814a4e7bf
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-13 11:43:44 +0800

    94 to 96 elif statement

--- Program01.py
+++ Program01.py
@@ -3,3 +3,6 @@ grade = float(input ("Grade Percentage: "))
 if grade >= 97 and grade <= 100:
     print("Grade/Mark: 1.0")
     print("Description: Excellent")
+elif grade >= 94 and grade <= 96:
+    print("Grade/Mark: 1.25")
+    print("Description: Excellent")
